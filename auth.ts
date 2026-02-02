451e6d9172566b410d06606b98732f77b2c5c1c0
Farid Ganbarov <17003685+faridnextjs@users.noreply.github.com>
2026-02-02 19:12:57 +0100

    configured Google OAuth + configure picturesin next.config.ts for Google profile picture

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       if (account?.provider === "google") {
         return (
           profile?.email_verified === true &&
-          profile?.email?.endsWith("@example.com") === true
+          profile?.email?.endsWith("faridnextjs@gmail.com") === true
         );
       }
       return true; // Do different verification for other providers that don't have `email_verified`
